89d01f73cc4a4dc0dc95b6f6b26fda2d3aaa2716
Gabriel Ribeiro Todt Ferreira <127440987+SoTrabalhoPUC@users.noreply.github.com>
2024-05-02 20:33:18 -0300

    Update Sentimento.java

--- Exercicio4/Sentimento.java
+++ Exercicio4/Sentimento.java
@@ -4,7 +4,7 @@ import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 import com.google.cloud.language.v1.Document.Type;
 
-public class SentimentAnalysisExample {
+public class Sentimento {
     public static void main(String[] args) throws Exception {
         String apiKey = "sua_chave_de_api_aqui";
         Scanner scanner = new Scanner(System.in);
